895adbb31f358c8a974d88cadb8ceafc7a4b2c7a
Kevin Chen <tianyuchen@umass.edu>
2025-03-07 10:34:38 -0500

    Added throughput predictors.

---
+++ tests/ThroughputPredictors/EMAPredictorTest.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+
+import ABRSimulation360.ThroughputPredictors.EMAPredictor;
+
+TEST(EMAPredictorTest, BasicPrediction) {
+    EMAPredictor predictor;
+
+    predictor.Update(4., 2.);
+    EXPECT_DOUBLE_EQ(predictor.PredictThroughputMbps(), 16.);
+
+    predictor.Update(16., 4);
+    EXPECT_NEAR(predictor.PredictThroughputMbps(), 27.6, 0.05);
+
+    predictor.Update(4., 4.);
+    EXPECT_NEAR(predictor.PredictThroughputMbps(), 12.9, 0.05);
+}
